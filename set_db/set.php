82295347fa870b209c4a8546f7d4b52ab6e01e1e
roomantsov <iromantsovv@gmail.com>
2016-03-29 22:04:46 +0300

    Upgraded databases
    Upgraded databases

--- set_db/set.php
+++ set_db/set.php
@@ -2,22 +2,22 @@
     require 'db_connection.php';
     $CONN->exec('CREATE TABLE
     users (
-        id INT NOT NULL AUTO_INCREMENT,
+        id SMALLINT NOT NULL AUTO_INCREMENT,
         email VARCHAR(100) NOT NULL UNIQUE,
         nickname TEXT NOT NULL,
         password TEXT NOT NULL,
-        role TEXT NOT NULL,
+        role VARCHAR(10) NOT NULL,
         PRIMARY KEY(id)
     )');
     $CONN->exec('CREATE TABLE
     notes (
-        id INT NOT NULL AUTO_INCREMENT,
+        id SMALLINT NOT NULL AUTO_INCREMENT,
         title TEXT NOT NULL,
         content TEXT NOT NULL,
         image TEXT NOT NULL,
-        author_id TEXT NOT NULL,
-        date TEXT NOT NULL,
-        access TEXT NOT NULL,
+        author_id TINYINT NOT NULL,
+        date VARCHAR(11) NOT NULL,
+        access VARCHAR(8) NOT NULL,
         PRIMARY KEY(id)
     )');
 
